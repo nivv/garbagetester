a4dc012693d66918da277ca87df138b1c352db65
Albin Nilsson <albin.nilsson89@gmail.com>
2015-08-01 10:16:04 +0200

    Commiting to new branch ya know

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5</div>
+                <div class="title">Laravel 5, this is the new branch</div>
             </div>
         </div>
     </body>
